aea5cbf7baef462fde627455125d5537ded940ab
Constantin Moreira <constantin@MacBook-Air-de-Constantin.local>
2026-05-12 19:57:36 +0200

    Add Music Splitter to sites list

--- src/data/sites.js
+++ src/data/sites.js
@@ -7,6 +7,7 @@ const sites = [
   { id: 'five', label: 'FIVE', url: 'https://cyanoide.github.io/FiveCRF/', emoji: '🎮', svg: 'FIVE.svg' },
   { id: 'ratp', label: 'Ratp RER', url: 'https://cyanoide.github.io/ratprer/', emoji: '🚇', svg: 'ratp.svg' },
   { id: 'looper', label: 'Looper Music', url: 'https://loopermusic.streamlit.app/', emoji: '🎵', svg: 'looper.svg' },
+  { id: 'musicsplitter', label: 'Music Splitter', url: 'https://cyanoide.github.io/Music-Splitter/', emoji: '🎚', svg: 'musicsplitter.svg' },
   { id: 'soundcloud', label: 'SoundCloud', url: 'https://soundcloud.com/cyanoide', emoji: '♪', svg: 'Soundcloud.svg' },
   { id: 'life', label: 'Life Tracker', url: 'https://cyanoide.github.io/Life-Tracker/', emoji: '❤', svg: 'life.svg' },
   { id: 'camera', label: 'Camera Map', url: 'https://cyanoide.github.io/CameraMap/', emoji: '📌', svg: 'cameramap.svg' },
